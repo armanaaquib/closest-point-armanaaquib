94a7db827e4c2761458a7184dda6488b92dd8690
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-04-12 20:35:22 +0530

    takes: Location of organism in main

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
+#include <stdio.h>
 #include "point.h"
 
 int main(void)
 {
+  Point food_points[] = FOOD_POINTS;
+  unsigned points_length = POINTS_LENGTH;
+  Point closest_food_location, current_location;
+  int x, y;
+
+  printf("Enter Location of organism(x, y): ");
+  scanf("%d,%d", &x, &y);
+
+  current_location = (Point){.x=x, .y=y};
+  get_closest_food(food_points, points_length, current_location, &closest_food_location);
+  
+  printf("Location of organism: [%d %d],  ", x, y);
+  printf("Closest food target : [%d %d]\n", closest_food_location.x, closest_food_location.y);
+
   return 0;
 }
